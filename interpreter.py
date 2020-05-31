bc363d219a45f09534ba18b2af5edb096596363a
iammpo <you@example.com>
2020-06-01 01:04:19 +0300

    path finding kinda works

--- interpreter.py
+++ interpreter.py
@@ -1,8 +1,200 @@
 import sys
+import copy
 import parser
+import random
+from time import sleep
+import os
+
 from typing import List, Optional, Union
-import robot
-	
+class Cell:
+    def __init__(self, x, y, type):
+        self.x = x
+        self.y = y
+        self.type = type
+
+class Robot:
+	def __init__(self, file, drons = 1000):
+		self.life = True
+		s = file.readline()
+		b=[]
+		self.x=int(s[0])
+		self.y=int(s[2])
+		c=file.read()
+		b=c.split('\n')
+		for i in range(len(b)):
+			#b[i] = b[i].split(',')
+			b[i] = list(b[i])
+			for j in range(len(b[i])):
+				if b[i][j] == '#':	b[i][j] = 1
+				if b[i][j] == '.':	b[i][j] = 2
+				if b[i][j] == '$':	b[i][j] = 3
+				if b[i][j] == '?':	b[i][j] = 0
+		self.map = b
+		self.drons = drons
+        
+	def show_map(self):
+		for i in range(len(self.map)):
+			for j in range(len(self.map[i])):
+				if i == self.x and j == self.y: #ROBOT
+					print("R", end='')
+				elif self.map[i][j] == 1: #WALL
+					print("#", end='')	
+				elif self.map[i][j] == 2: #EMPTY
+					print(".", end='')
+				elif self.map[i][j] == 3: #EXIT
+					print("$", end='')
+				else: #UNDEF
+					print("?", end='')
+			print()
+			
+	def up(self, n):
+		for i in range(n):
+			if self.life:
+				self.x-=1
+				if self.x<0 or self.x>=len(self.map):
+					self.life=False
+				elif self.map[self.x][self.y] == 1:
+					self.life=False
+		self.show_map()
+		sleep(0.2)
+		os.system('clear')
+
+	def down(self, n):
+		for i in range(n):
+			if self.life:
+				self.x+=1
+				if self.x<0 or self.x>=len(self.map):
+					self.life=False
+				elif self.map[self.x][self.y] == 1:
+					self.life=False
+		self.show_map()
+		sleep(0.2)
+		os.system('clear')
+
+
+	def left(self, n):
+		for i in range(n):
+			if self.life:
+				self.y-=1
+				if self.y<0 or self.y>=len(self.map[0]):
+					self.life=False
+				if self.map[self.x][self.y] == 1:
+					self.life=False
+		self.show_map()
+		sleep(0.2)
+		os.system('clear')
+
+
+	def right(self, n):
+		for i in range(n):
+			if self.life:
+				self.y+=1
+				if self.y<0 or self.y>=len(self.map[0]):
+					self.life=False
+				if self.map[self.x][self.y] == 1:
+					self.life=False
+		self.show_map()
+		sleep(0.2)
+		os.system('clear')
+
+
+	def drons_count(self):
+		#print(self.drons)
+		return self.drons
+    
+	def send_drons(self, n):
+		self.drons-=n
+		new_map=[]
+		array=[0]*121
+		for i in range(n):
+			a=Satellite(self.x, self.y, self.map)
+			new_map.append(a.exploring())    
+		for row in new_map:
+			for cell in row:
+				a=cell.x-self.x+5
+				b=cell.y-self.y+5
+				array[a*11+b]=cell.type
+		return array
+        
+        
+
+    
+class Satellite:
+	def __init__(self, x, y, _map):
+		self.life = True
+		self.x = x
+		self.y = y
+		self.map = _map
+		self.new_map = []
+	def show_map(self):
+		for i in range(len(self.map)):
+			for j in range(len(self.map[0])):
+				if i == self.x and j == self.y:
+					print("0", end='') #Robot
+				elif self.map[i][j] == 1: #Wall
+					print("*", end='')	
+				elif self.map[i][j]  ==3: #Exit
+					print("X", end='')
+				else:          #Empty
+					print(" ", end='')			
+			print()
+	def up(self):
+		self.x-=1
+		if (self.x<0 or self.x>=len(self.map)):
+			self.life=False
+			self.new_map.append(Cell(self.x,self.y,1))
+		else:
+			if self.map[self.x][self.y] == 1:
+				self.life=False
+			self.new_map.append(Cell(self.x,self.y,self.map[self.x][self.y]))
+		#self.show_map()
+	def down(self):
+		self.x+=1
+		if (self.x<0 or self.x>=len(self.map)):
+			self.life=False
+			self.new_map.append(Cell(self.x,self.y,1))
+		else:
+			if self.map[self.x][self.y] == 1:
+				self.life=False
+			self.new_map.append(Cell(self.x,self.y,self.map[self.x][self.y]))
+		#self.show_map()
+	def left(self):
+		self.y-=1
+		if (self.y<0 or self.y>=len(self.map)):
+			self.life=False
+			self.new_map.append(Cell(self.x,self.y,1))
+		else:
+			if self.map[self.x][self.y] == 1:
+				self.life=False
+			self.new_map.append(Cell(self.x,self.y,self.map[self.x][self.y]))
+		#self.show_map()
+	def right(self):
+		self.y+=1
+		if (self.y<0 or self.y>=len(self.map)):
+			self.life=False
+			self.new_map.append(Cell(self.x,self.y,1))
+		else:
+			if self.map[self.x][self.y] == 1:
+				self.life=False
+			self.new_map.append(Cell(self.x,self.y,self.map[self.x][self.y]))
+		#self.show_map()
+	def exploring(self):
+		steps=random.randint(1,5)
+		for i in range(steps):
+			if self.life:
+				step=random.randint(1,4)
+				if step == 1:
+					self.up()
+				elif step == 2:
+					self.down()
+				elif step == 3:
+					self.left()
+				else:
+					self.right()
+			else:
+				break
+		return self.new_map
+
 class Variable:
 	#перегрузка операторов:
 	def __init__(self, symtype='var', value=None, const_flag=False, dim=0,dims=[]):
@@ -57,14 +249,14 @@ class Variable:
 		
 class Interpreter:
 
-	def __init__(self, filename):
+	def __init__(self,file):
 		self.parser = parser.Parser()
 		self.program = None
 		self.symbol_table = [dict()]
 		self.functions = dict()
 		self.tree = None
 		self.scope = 0
-		self.robot = robot.Robot(filename)
+		self.robot=Robot(file)
 
 	def interpreter(self, prog=None):
 		self.prog = prog
@@ -73,22 +265,17 @@ class Interpreter:
 			#self.interpreter_tree(self.tree)
 			self.interpreter_node(self.tree)
 			if 'main' not in self.functions.keys():
-				sys.stderr.write(f'error: no main function\n')
+				sys.stderr.write(f'error: no main function\n ')
 				return
 			else:
-				self.main_function()
-				#self.interpreter_node(self.functions['main'])
+				self.scope=1
+				self.symbol_table.append(dict())
+				statements = self.functions['main']
+				if statements is not None:
+					self.interpreter_node(statements)
+				
 		else:
 			sys.stderr.write(f'Can\'t intemperate this, incorrect syntax\n')
-			
-	def main_function(self):
-		self.scope += 1
-		self.symbol_table.append(dict())
-		statements = self.functions['main']
-		if statements is not None:
-			self.interpreter_node(statements)   
-		self.scope -= 1 
-		self.symbol_table.pop()
 
 	def interpreter_tree(self, tree):
 		print("Program tree:\n")
@@ -99,40 +286,53 @@ class Interpreter:
 		if node is None:
 			return
 		if node.type == 'program':
+			#print(node.lineno)
 			self.interpreter_node(node.children)
 		elif node.type == 'blocks' or node.type == 'conditions' or node.type == 'statements' or node.type == 'declarations':
+			#print(node.lineno)
 			for child in node.children: 
 				self.interpreter_node(child)
 		elif node.type == 'vardeclaration' or node.type == 'switch':
+			#print(node.lineno)
 			self.interpreter_node(node.children)
 		elif node.type == 'declaration_var':
 			name = node.children.value
+			#print(node.lineno)
 			if (name in self.symbol_table[self.scope].keys()) or (name in self.symbol_table[0].keys()):
 				sys.stderr.write(f'error: redeclaration of variable {name}\n')
 				return
 			else:
 				self.symbol_table[self.scope][name] = Variable(node.value.value, None, False)
 		elif node.type == 'declaration_var_init':
+			#print(node.lineno)
 			self.initialization(node, False)
 		elif node.type == 'declaration_var_const':
+			#print(node.lineno)
 			self.initialization(node, True)
 		elif node.type == 'declaration_array':
+			#print(node.lineno)
 			self.array_declaration(node, False)
 		elif node.type == 'declaration_array_init': 
+			#print(node.lineno)
 			self.array_initialization(node, False)
 		elif node.type == 'declaration_array_const':
+			#print(node.lineno)
 			self.array_initialization(node, True)
 		elif node.type == 'function':
+			#print(node.lineno)
 			if node.value.value not in self.functions.keys():
 				self.functions[node.value.value] = node.children #занесли в словарь имя функции : statements
 				#self.interpreter_node(node.children)              
 			else:                                                        
 				sys.stderr.write(f'error: redeclaration of function {node.value.value}\n')
 		elif node.type == 'function_call': # можно ли вызывать main?
+			#print(node.lineno)
 			self.function_call(node)
 		elif node.type == 'assignment':
+			#print(node.lineno)
 			self.assignment(node)
 		elif node.type == 'variables' or node.type == 'dimensions' or node.type == 'indexes' or node.type == 'values': 
+			#print(node.lineno)
 			variables = []
 			for child in node.children:
 				var = self.interpreter_node(child)
@@ -141,14 +341,18 @@ class Interpreter:
 				else: return None
 			return variables
 		elif node.type == 'variable':
+			#print(node.lineno)
 			name = node.value.value
+			##print(name)
 			return self.find_variable(name)
 		elif node.type == 'variable_array':
+			#print(node.lineno)
 			name = node.value.value
 			indexes = self.interpreter_node(node.children)
 			return self.find_elem_of_array(name, indexes)
 		elif node.type == 'const':
 			value = node.value
+			#print(node.lineno)
 			if isinstance(value, int):
 				return Variable('INT', value, True)
 			elif value == 'FALSE':
@@ -156,12 +360,9 @@ class Interpreter:
 			elif value == 'TRUE':
 				return Variable('BOOL', True, True)
 			else:
-				if value == 'UNDEF':	value = 0
-				elif value == 'WALL':	value = 1
-				elif value == 'EMPTY':	value = 2
-				elif value == 'EXIT':	value = 3
 				return Variable('CELL', value, True) #EMPTY/WALL/EXIT/UNDEF
 		elif node.type == 'expressions':
+			#print(node.lineno)
 			expressions = [] 
 			for child in node.children:
 				expr = self.interpreter_node(child)
@@ -171,8 +372,10 @@ class Interpreter:
 					else:
 						expressions.append(expr)
 				else: return None
+			#print(expressions)
 			return expressions #проверить не может ли вернуться пустой список
 		elif node.type == 'dimension':
+			#print(node.lineno)
 			expr = self.interpreter_node(node.children)
 			if isinstance(expr,list):
 				expr = expr[0]
@@ -184,6 +387,7 @@ class Interpreter:
 				return
 			return [int(expr.value)]
 		elif node.type == 'index':
+			#print(node.lineno)
 			expr = self.interpreter_node(node.children) 
 			if isinstance(expr,list):
 				expr = expr[0]
@@ -195,6 +399,7 @@ class Interpreter:
 				return
 			return [int(expr.value)]
 		elif node.type == 'value':
+			#print(node.lineno)
 			expr = self.interpreter_node(node.children)
 			if isinstance(expr,list):
 				expr = expr[0]
@@ -203,6 +408,7 @@ class Interpreter:
 				return
 			return [expr]	
 		elif node.type == 'condition':
+			#print(node.lineno)
 			condition = self.interpreter_node(node.children['condition'])
 			if isinstance(condition,list):
 				condition = condition[0]
@@ -215,6 +421,7 @@ class Interpreter:
 			if bool(condition.value) is True:
 				self.interpreter_node(node.children['body'])
 		elif node.type == 'while':
+			#print(node.lineno)
 			while True:
 				condition = self.interpreter_node(node.children['condition'])
 				if isinstance(condition,list):
@@ -230,6 +437,7 @@ class Interpreter:
 				else:
 					break
 		elif node.type == 'math': #проверять expression на немассивность
+			#print(node.lineno)
 			if node.value == '<ADD>':
 				return self.addition(node.children)
 			elif node.value == '<MUL>':
@@ -250,35 +458,41 @@ class Interpreter:
 				return self.logic_and(node.children)
 			elif node.value == '<NOT>':
 				return self.logic_not(node.children)
-		elif node.type == 'operator': #проверять expression на немассивность и на не CELL
+		elif node.type == 'operator':
+			#print(node.lineno)
 			if node.value == '<LEFT>':
 				n=self.interpreter_node(node.children)
-				self.robot.left(int(n.value))
+				self.robot.left(n.value)
 			elif node.value == '<RIGHT>':
 				n=self.interpreter_node(node.children)
-				self.robot.right(int(n.value))
+				self.robot.right(n.value)
 			elif node.value == '<UP>':
 				n=self.interpreter_node(node.children)
-				self.robot.up(int(n.value))
+				self.robot.up(n.value)
 			elif node.value == '<DOWN>':
 				n=self.interpreter_node(node.children)
-				self.robot.down(int(n.value))
+				self.robot.down(n.value)
 			elif node.value == '<GETDRONSCOUNT>':
 				var = self.interpreter_node(node.children)
 				if isinstance(var,list):
 					var = var[0]
+				n=self.robot.drons_count()
 				if var.dim !=0 or var.type != 'INT':
-					sys.stderr.write(f'error: GETDRONSCOUNT returns INT type not-array variable\n')
+					sys.stderr.write(f'error: assignment error\n')
 					return
-				var.value=self.robot.drons #проверить на константность
-		elif node.type == 'senddrons': #проверять expression на немассивность и на не CELL
+				var.value=n
+		elif node.type == 'senddrons':
+			#print(node.lineno)
 			n=self.interpreter_node(node.children)
-			array=self.robot.send_drons(int(n.value))
-			cells = []
-			for value in array:
-				cells.append(Variable('CELL', value, True))
+			array=self.robot.send_drons(n.value)
+			#print(array)
+			cells=[]
+			a=['UNDEF','WALL','EMPTY','EXIT']
+			for i in array:
+				cells.append(Variable('CELL', a[i], False))
+			#print(cells)
 			return Variable('CELL', cells, True, 2, [11,11])
-				
+
 	def assignment(self, node: parser.SyntaxTreeNode): 
 		variables = self.interpreter_node(node.children[1])
 		if variables is None:
@@ -314,7 +528,7 @@ class Interpreter:
 					sys.stderr.write(f'error: different sizes of arrays in assignment\n')
 					return
 				#преобразование типов для массивов
-				var.value = expression.value
+				var.value = copy.deepcopy(expression.value)
 		else:
 			sys.stderr.write(f'error: cannot assign array to not array and vice versa(i naoborot)\n')
 			return
@@ -332,6 +546,8 @@ class Interpreter:
 			sys.stderr.write(f'error: variable {name} is not an array\n')
 			return
 		i = var[0].index_recount(indexes)
+		if var[0].const_flag is True:
+			var[0].value[i].const_flag=True
 		return [var[0].value[i]]
 	
 	def bool_to_int(self, var: Variable):#для массивов сделать
@@ -393,7 +609,10 @@ class Interpreter:
 		size=1
 		for i in dimensions:
 			size*=i
-		self.symbol_table[self.scope][name] = Variable(vartype, [Variable(vartype, None, flag)]*size, flag, dim, dimensions)
+		values=[]
+		for i in range(size):
+			values.append(Variable(vartype, None, flag))
+		self.symbol_table[self.scope][name] = Variable(vartype, values, flag, dim, dimensions)
 		
 	def array_initialization(self, node: parser.SyntaxTreeNode, flag: bool):
 		vartype = node.value.value
@@ -409,7 +628,7 @@ class Interpreter:
 			return
 		values = self.interpreter_node(node.children[3])
 		for i in range(len(values)):
-			values[i] = self.type_conversion(Variable(vartype, None, flag), values[i])
+			values[i] = self.type_conversion(Variable(vartype, None, False), values[i])
 			if values[i] is None: 
 				sys.stderr.write(f'error: initialization error of variable {name}\n')
 				return
@@ -566,7 +785,7 @@ class Interpreter:
 			sys.stderr.write(f'error: recursion\n')
 			return
 		funcname = node.value.value
-		print(f'{funcname}\n')
+		#print(funcname)
 		if funcname not in self.functions.keys():
 			sys.stderr.write(f'error: call of undeclarated function\n')
 			return
@@ -575,29 +794,22 @@ class Interpreter:
 		statements = self.functions[funcname]
 		if statements is not None:
 			self.interpreter_node(statements)   
-		self.scope -= 1 #хз
-		interpreter.print_symbol()
 		self.symbol_table.pop()
-		
+		#print(self.scope)
+		self.scope -= 1
 	def print_symbol(self):
 		print(self.symbol_table)
 		
 if __name__ == '__main__':
-	mapp=open('map','r')
-	interpreter = Interpreter(mapp)
-	interpreter.robot.show_map()
-	f=open('test(arrays)','r')
+	map=open('map_2','r')
+	interpreter = Interpreter(map)
+	f=open('path_finding','r')
 	interpreter.interpreter(f.read())
 	f.close()
-	print(f'life status: {interpreter.robot.life}')
 	interpreter.print_symbol()
+	interpreter.robot.show_map()
+	print(f'robot has {interpreter.robot.drons} drones left')
+	print(f'life status is {interpreter.robot.life}')
 	
-						
-
-
-
-
-
-
-
-
+	
+	
